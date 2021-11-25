8d8713856a54ef7a91f2ed3759eab9b4a02208c8
sc-stack <74339428+sc-stack@users.noreply.github.com>
2021-11-25 03:49:06 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = '';
 const TOTAL_MINT_COUNT = 50;
 
-// I moved the contract address to the top for easy access.
 const CONTRACT_ADDRESS = "0xAC5c810a36516f72A13C03Ce20140E1F48232A3D";
 
 const App = () => {
@@ -33,7 +32,7 @@ const App = () => {
           console.log("Found an authorized account:", account);
 					setCurrentAccount(account)
           
-          // Setup listener! This is for the case where a user comes to our site
+          //event listener setup
           // and ALREADY had their wallet connected + authorized.
           setupEventListener()
       } else {
@@ -55,8 +54,8 @@ const App = () => {
       console.log("Connected", accounts[0]);
       setCurrentAccount(accounts[0]);
 
-      // Setup listener! This is for the case where a user comes to our site
-      // and connected their wallet for the first time.
+      // event listener
+      // connect wallet for the first time
       setupEventListener() 
     } catch (error) {
       console.log(error)
@@ -65,19 +64,16 @@ const App = () => {
 
   // Setup our listener.
   const setupEventListener = async () => {
-    // Most of this looks the same as our function askContractToMintNft
+    // same as function askContractToMintNft
     try {
       const { ethereum } = window;
 
       if (ethereum) {
-        // Same stuff again
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
         const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
 
-        // THIS IS THE MAGIC SAUCE.
-        // This will essentially "capture" our event when our contract throws it.
-        // If you're familiar with webhooks, it's very similar to that!
+	//capture event
         connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
           console.log(from, tokenId.toNumber())
           alert(`Hey there! We've minted your NFT and sent it to your wallet. It may be blank right now. It can take a max of 10 min to show up on OpenSea. Here's the link: https://testnets.opensea.io/assets/${CONTRACT_ADDRESS}/${tokenId.toNumber()}`)
